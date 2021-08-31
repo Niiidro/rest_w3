89d9c9052f1b9743cba8286e5a5494cbb3ed8193
Philipp Minder <philipp.minder@hotmail.com>
2021-08-31 19:30:21 +0200

    Create Static Model

---
+++ Models/user.model.js
@@ -0,0 +1,18 @@
+let users = {
+  1: {
+    id: "1",
+    name: "Joerg",
+    nachname: "Meier",
+    email: "jmeier@hf-ict.info",
+  },
+  2: {
+    id: "2",
+    name: "Valentin",
+    nachname: "Amstutz",
+    email: "vamstutz@hf-ict.info",
+  },
+};
+
+export default {
+  users
+}
