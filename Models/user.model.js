2b2ac4821e0cac6c30898cd7d449de3efc210338
Philipp Minder <philipp.minder@hotmail.com>
2021-08-31 19:56:36 +0200

    Create Empty Functions

--- Models/user.model.js
+++ Models/user.model.js
@@ -1,18 +1,28 @@
+import { v4 as uuidv4 } from "uuid";
+
 let users = {
   1: {
-    id: "1",
+    id: uuidv4(),
     name: "Joerg",
     nachname: "Meier",
     email: "jmeier@hf-ict.info",
   },
   2: {
-    id: "2",
+    id: uuidv4(),
     name: "Valentin",
     nachname: "Amstutz",
     email: "vamstutz@hf-ict.info",
   },
 };
 
-export default {
-  users
+function createUser() {}
+function getAllUsers() {}
+function getUserById(id) {}
+function updateUser(id, method) {
+  //method = Put oder Patch
 }
+function deleteUser(id) {}
+
+export default {
+  users,
+};
--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 import express from "express";
-import users from "./Models/user.model.js"
+import users from "./Models/user.model.js";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send(users);
+//Gibt alle Personen aus
+app.get("/", (req, res) => {});
+//Gibt eine Person mit der jeweiligen ID aus
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+});
+//Erstelle eine Person
+app.post("/", (req, res) => {
+  const body = req.body;
+});
+//Überschreibt eine Person
+app.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const method = "put";
+});
+//Aktualisert eine Person
+app.patch("/:id", (req, res) => {
+  const id = req.params.id;
+  const method = "patch";
+});
+//Löscht eine Person
+app.delete("/:id", (req, res) => {
+  const id = req.params.id;
 });
 
 app.listen(port, () => {
