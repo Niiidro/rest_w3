11e4a7bab0890cc55d1aea0e194b51fac2a9b92a
HF-Schaffner <73192155+HF-Schaffner@users.noreply.github.com>
2021-09-01 08:11:16 +0200

    Update users.js
    all function finished

--- users.js
+++ users.js
@@ -1,16 +1,19 @@
 import { v4 as uuidv4 } from "uuid";
+
+// Klasse User
 class User {
-  constructor(body) {
-    this.id = uuidv4();
+  constructor(body,id) {
     this.name = body.name;
     this.nachname = body.nachname;
     this.email = body.email;
+    this.id = uuidv4();
   }
 }
 
+// Beispiel daten initialisieren
 let users = [
   {
-    id: "1", //uuidv4(),
+    id: uuidv4(), // Random ID verwenden
     name: "Joerg",
     nachname: "Meier",
     email: "jmeier@hf-ict.info",
@@ -24,21 +27,50 @@ let users = [
 ];
 
 function createUser(body) {
-  const user = new User(body);
-  users.push(user);
+  // Neues User Objekt erstellen mit dem body 
+  const user = new User(body,null);
+  users.push(user); // neues Objekt zum Array hinzufügen
   return users;
 }
 function getAllUsers() {
   return users;
 }
 function getUserById(id) {
+  // Sucht den Benutzer ung gibt diesen zurück
   return users.find((x) => x.id === id);
 }
-function updateUser(id, method) {
+function updateUser(id,body,method) {
   //method = Put oder Patch
-}
-function deleteUser(id) {}
+  let currentUser = users.find((x) => x.id === id);
+  // put Objekt ersetzen wenn Felder leer sind Objekt ohne diese Felder generieren
+  if(method == 'put'){
+      currentUser.name = body.name
+      currentUser.nachname = body.nachname
+      currentUser.email = body.email
+  // patch nur Felder anpassen welche geändert haben
+  }else if(method == 'patch'){
+    // auf null prüfen damit leere Felder übersprungen werden
+    if (body.name != null){ 
+      currentUser.name = body.name
+    }
+    if (body.nachname != null){
+      currentUser.nachname = body.nachname
+    }
+    if (body.email != null){
+      currentUser.email = body.email
+    }
+  }
+  return users
 
+}
+function deleteUser(id) {
+  // User Index (stelle im Array) suchen
+  let index = users.findIndex(x => x.id === id);
+  users.splice(index,1); // Eintrag löschen ,1 = 1 Element löschen
+  return users
+}
+
+// Export der funktionen
 export default {
   users,
   createUser,
