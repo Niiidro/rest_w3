c71b0ca52f4db072218c81e34d81e2595231b0ca
HF-Schaffner <73192155+HF-Schaffner@users.noreply.github.com>
2021-09-05 15:38:20 +0200

    HTTP Status hinzugefügt

--- users.js
+++ users.js
@@ -29,24 +29,39 @@ let users = [
 function createUser(body) {
   // Neues User Objekt erstellen mit dem body 
   const user = new User(body,null);
-  users.push(user); // neues Objekt zum Array hinzufügen
+  users.push(user) // neues Objekt zum Array hinzufügen
   return users;
 }
+
 function getAllUsers() {
   return users;
 }
+
 function getUserById(id) {
-  // Sucht den Benutzer ung gibt diesen zurück
-  return users.find((x) => x.id === id);
+  // Sucht den Benutzer und gibt diesen zurück
+  const user = users.find((x) => x.id === id);
+  if(user == null){
+    return 404; // kein User gefunden 404 status 
+  }
+  return user;  // User gefunden Objekt zurückgeben
 }
+
 function updateUser(id,body,method) {
   //method = Put oder Patch
   let currentUser = users.find((x) => x.id === id);
+  if (currentUser == null){
+    return [404, 'User not found']; // User nicht gefunden
+  }
+  
   // put Objekt ersetzen wenn Felder leer sind Objekt ohne diese Felder generieren
   if(method == 'put'){
+    if(body.name == null || body.nachname == null || body.email == null){ // Put müssen alle infos ausgefüllt sein
+      return [204, 'Userinfos incomplete']
+    }
       currentUser.name = body.name
       currentUser.nachname = body.nachname
       currentUser.email = body.email
+
   // patch nur Felder anpassen welche geändert haben
   }else if(method == 'patch'){
     // auf null prüfen damit leere Felder übersprungen werden
@@ -61,11 +76,16 @@ function updateUser(id,body,method) {
     }
   }
   return users
-
 }
+
 function deleteUser(id) {
+  const user = users.find((x) => x.id === id);
+  if(user == null){
+    return 404; // kein User gefunden also 404 status 
+  }
   // User Index (stelle im Array) suchen
   let index = users.findIndex(x => x.id === id);
+  
   users.splice(index,1); // Eintrag löschen ,1 = 1 Element löschen
   return users
 }
